cc92c431f6da5a96ca27d2de4fda27385bddd09a
duckycoding-dev <duckycoding.dev@gmail.com>
2025-02-11 00:29:52 +0100

    setup hono build

--- services/backend/src/hc.ts
+++ services/backend/src/hc.ts
@@ -6,6 +6,5 @@ import { hc } from 'hono/client';
 // this is a trick to calculate the type when compiling
 const client = hc<typeof app>('');
 export type Client = typeof client;
-
 export const hcWithType = (...args: Parameters<typeof hc>): Client =>
   hc<typeof app>(...args);
